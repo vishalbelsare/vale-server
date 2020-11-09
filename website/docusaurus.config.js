1b18475fb0ac8199fa9ecdb2cbb3d59a1653c7f5
Joseph Kato <joseph@jdkato.io>
2020-11-08 20:34:03 -0800

    Update 'About'

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,104 +1,107 @@
 module.exports = {
-  title: 'errata.ai',
-  tagline: 'Innovative tools for collaborative writing',
-  url: 'https://nervous-almeida-4ca728.netlify.app',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'errata-ai', // Usually your GitHub org/user name.
-  projectName: 'vale-server', // Usually your repo name.
+  title: "errata.ai",
+  tagline: "Innovative tools for collaborative writing",
+  url: "https://nervous-almeida-4ca728.netlify.app",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "errata-ai", // Usually your GitHub org/user name.
+  projectName: "vale-server", // Usually your repo name.
 
-  plugins: [
-  ],
-  scripts: [
-  ],
-  stylesheets: [
-  ],
+  plugins: [],
+  scripts: [],
+  stylesheets: [],
 
   themeConfig: {
+    /*announcementBar: {
+      id: "supportus",
+      content:
+        '⭐️ If you like Docusaurus, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/facebook/docusaurus">GitHub</a>! ⭐️',
+    },*/
     prism: {
-      theme: require('prism-react-renderer/themes/github'),
-      darkTheme: require('./src/plugins/prism_themes/github'),
-      additionalLanguages: ['ini'],
+      theme: require("prism-react-renderer/themes/github"),
+      darkTheme: require("./src/plugins/prism_themes/github"),
+      additionalLanguages: ["ini"],
     },
     algolia: {
-      apiKey: '12bdf8cd642ab554d424ff89eed88ce9',
-      indexName: 'errata_ai'
+      apiKey: "12bdf8cd642ab554d424ff89eed88ce9",
+      indexName: "errata_ai",
     },
     navbar: {
       hideOnScroll: false,
-      title: 'errata.ai | Docs',
+      title: "errata.ai | Docs",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo-alt.svg',
+        alt: "My Site Logo",
+        src: "img/logo-alt.svg",
       },
       items: [
         {
-          label: 'Products',
-          position: 'left', // or 'right'
-          items: [{
-              label: 'Vale',
-              to: '/vale/about',
+          label: "Products",
+          position: "left", // or 'right'
+          items: [
+            {
+              label: "Vale",
+              to: "/vale/about",
             },
             {
-              label: 'Vale Server',
-              to: '/vale-server/install',
+              label: "Vale Server",
+              to: "/vale-server/install",
             },
             {
-              label: 'Static School',
-              href: 'https://staticschool.com/',
+              label: "Static School",
+              href: "https://staticschool.com/",
             },
           ],
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "blog", label: "Blog", position: "left" },
 
         // {href: '/api/index.html', label: 'API', position: 'left'},
 
         {
-          href: 'https://github.com/errata-ai',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
+          href: "https://github.com/errata-ai",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
       links: [
         {
-          title: 'About',
+          title: "About",
           items: [
             {
-              label: 'Home',
-              to: 'https://errata.ai',
+              label: "Home",
+              to: "https://errata.ai",
             },
             {
-              label: 'Blog',
-              to: 'Blog',
+              label: "Blog",
+              to: "Blog",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/errata-ai',
+              label: "GitHub",
+              href: "https://github.com/errata-ai",
             },
             {
-              label: 'Slack',
-              href: 'https://writethedocs.slack.com/archives/CBWQQ5E57',
+              label: "Slack",
+              href: "https://writethedocs.slack.com/archives/CBWQQ5E57",
             },
             {
-              label: 'Open Collective',
-              href: 'https://opencollective.com/vale',
+              label: "Open Collective",
+              href: "https://opencollective.com/vale",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Become a Sponsor',
-              to: 'https://opencollective.com/vale',
+              label: "Become a Sponsor",
+              to: "https://opencollective.com/vale",
             },
           ],
         },
@@ -108,26 +111,26 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           // It is recommended to set document id as docs home page (`docs/` path).
           // homePageId: 'about',
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/errata-ai/vale-server/edit/master/website/',
+            "https://github.com/errata-ai/vale-server/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/errata-ai/vale-server/edit/master/website/blog/',
+            "https://github.com/errata-ai/vale-server/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        }
+          customCss: require.resolve("./src/css/custom.css"),
+        },
       },
     ],
   ],
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,99 +1,88 @@
 module.exports = {
-  'vale-server': {
+  "vale-server": {
     Introduction: [
-        'vale-server/install',
-        'vale-server/gui',
-        'vale-server/usage'
+      "vale-server/install",
+      "vale-server/gui",
+      "vale-server/usage",
     ],
     Concepts: [
-        'vale-server/concepts/styles',
-        'vale-server/concepts/scoping',
-        'vale-server/concepts/config',
-        'vale-server/concepts/syncing'
+      "vale-server/concepts/styles",
+      "vale-server/concepts/scoping",
+      "vale-server/concepts/config",
+      "vale-server/concepts/syncing",
     ],
-    Guides: [
-        'vale-server/guides/languages'
-    ],
+    Guides: ["vale-server/guides/languages"],
     Clients: [
-        'vale-server/clients/atom',
-        'vale-server/clients/chrome',
-        'vale-server/clients/google-docs',
-        'vale-server/clients/sublime-text',
-        'vale-server/clients/vscode'
+      "vale-server/clients/atom",
+      "vale-server/clients/chrome",
+      "vale-server/clients/google-docs",
+      "vale-server/clients/sublime-text",
+      "vale-server/clients/vscode",
     ],
-    'Add-ons': [
-        'vale-server/add-ons/languagetool',
-    ],
+    "Add-ons": ["vale-server/add-ons/languagetool"],
     Resources: [
-        {
-            type: 'link',
-            label: 'Changelog',
-            href: 'https://headwayapp.co/errata-ai-release-notes'
-        },
-        {
-            type: 'link',
-            label: 'API Explorer',
-            href: 'https://docs.errata.ai/api/index.html'
-        },
-        {
-            type: 'link',
-            label: 'Style Library',
-            href: 'https://github.com/errata-ai/styles'
-        }
-    ]
+      {
+        type: "link",
+        label: "Changelog",
+        href: "https://headwayapp.co/errata-ai-release-notes",
+      },
+      {
+        type: "link",
+        label: "API Explorer",
+        href: "https://docs.errata.ai/api/index.html",
+      },
+      {
+        type: "link",
+        label: "Style Library",
+        href: "https://github.com/errata-ai/styles",
+      },
+    ],
   },
   vale: {
-      Introduction: [
-          'vale/about',
-          'vale/install',
-          'vale/cli'
-      ],
-      Concepts: [
-          'vale/styles',
-          'vale/vocab',
-          'vale/scoping',
-          'vale/config'
-      ],
-      Integrations: [
-          {
-              type: 'link',
-              label: 'GitHub Actions',
-              href: 'https://github.com/errata-ai/vale-action'
-          },
-          {
-              type: 'link',
-              label: 'Visual Studio Code',
-              href: 'https://github.com/errata-ai/vale-vscode'
-          },
-      ],
-      Guides: [
-          {
-              type: 'link',
-              label: 'Introducing Vale, an NLP-powered linter for prose',
-              href: 'https://medium.com/@jdkato/introducing-vale-an-nlp-powered-linter-for-prose-63c4de31be00'
-          },
-          {
-              type: 'link',
-              label: 'The Vale Workflow',
-              href: 'https://medium.com/@jdkato/the-vale-workflow-3b709fa39212'
-          },
-          {
-              type: 'link',
-              label: 'Vale & The OpenAPI Specification',
-              href: 'https://medium.com/@jdkato/vale-the-openapi-specification-8a7cfae135fb'
-          }
-      ],
-      Resources: [
-          {
-              type: 'link',
-              label: 'Style Library',
-              href: 'https://github.com/errata-ai/styles'
-          },
-          {
-              type: 'link',
-              label: 'Community Styles',
-              href: 'https://github.com/topics/vale-linter-style'
-          }
-      ]
+    Introduction: ["vale/about", "vale/install", "vale/cli"],
+    Concepts: ["vale/styles", "vale/vocab", "vale/scoping", "vale/config"],
+    Integrations: [
+      {
+        type: "link",
+        label: "GitHub Actions",
+        href: "https://github.com/errata-ai/vale-action",
+      },
+      {
+        type: "link",
+        label: "Visual Studio Code",
+        href: "https://github.com/errata-ai/vale-vscode",
+      },
+    ],
+    Guides: [
+      {
+        type: "link",
+        label: "Introducing Vale, an NLP-powered linter for prose",
+        href:
+          "https://medium.com/@jdkato/introducing-vale-an-nlp-powered-linter-for-prose-63c4de31be00",
+      },
+      {
+        type: "link",
+        label: "The Vale Workflow",
+        href: "https://medium.com/@jdkato/the-vale-workflow-3b709fa39212",
+      },
+      {
+        type: "link",
+        label: "Vale & The OpenAPI Specification",
+        href:
+          "https://medium.com/@jdkato/vale-the-openapi-specification-8a7cfae135fb",
+      },
+    ],
+    Resources: [
+      {
+        type: "link",
+        label: "Style Library",
+        href: "https://github.com/errata-ai/styles",
+      },
+      {
+        type: "link",
+        label: "Community Styles",
+        href: "https://github.com/topics/vale-linter-style",
+      },
+    ],
   },
 };
