94820780d4bdacfb055c9593b1388624769afc85
Joseph Kato <joseph@jdkato.io>
2020-12-05 21:17:24 -0800

    More color updates

--- website/src/plugins/prism_themes/github/index.js
+++ website/src/plugins/prism_themes/github/index.js
@@ -1,55 +1,60 @@
 var theme = {
-        "plain": {
-            "color": "#fff",
-            "backgroundColor": "#1e1e1e"
-        },
-        "styles": [{
-            "types": ["comment", "prolog", "doctype", "cdata"],
-            "style": {
-                "color": "rgb(161, 155, 143)",
-                "fontStyle": "italic"
-            }
-        }, {
-            "types": ["namespace"],
-            "style": {
-                "opacity": 0.7
-            }
-        }, {
-            "types": ["string", "attr-value"],
-            "style": {
-                "color": "rgb(33, 175, 144)"
-            }
-        }, {
-            "types": ["punctuation", "operator"],
-            "style": {
-                "color": "#393A34"
-            }
-        }, {
-            "types": ["entity", "url", "symbol", "number", "boolean", "variable", "constant", "property", "regex", "inserted"],
-            "style": {
-                "color": "rgb(102, 181, 246)"
-            }
-        }, {
-            "types": ["atrule", "keyword", "attr-name", "selector"],
-            "style": {
-                "color": "rgb(243, 153, 97)"
-            }
-        }, {
-            "types": ["function", "deleted", "tag"],
-            "style": {
-                "color": "rgb(187, 151, 231)"
-            }
-        }, {
-            "types": ["function-variable"],
-            "style": {
-                "color": "#6f42c1"
-            }
-        }, {
-            "types": ["tag", "selector", "keyword"],
-            "style": {
-                "color": "rgb(243, 153, 97)"
-            }
-        }]
-}
+  plain: {
+    color: "#ffffff",
+    backgroundColor: "#193549",
+  },
+  styles: [
+    {
+      types: ["comment", "punctuation"],
+      style: {
+        color: "rgb(0, 136, 255)",
+        fontStyle: "italic",
+      },
+    },
+    {
+      types: ["constant"],
+      style: {
+        color: "rgb(255, 98, 140)",
+      },
+    },
+    {
+      types: ["string"],
+      style: {
+        color: "rgb(165, 255, 144)",
+      },
+    },
+    {
+      types: ["builtin"],
+      style: {
+        color: "rgb(255, 157, 0)",
+      },
+    },
+    {
+      types: ["variable"],
+      style: {
+        color: "rgb(225, 239, 255)",
+      },
+    },
+    {
+      types: ["attr-name"],
+      style: {
+        color: "rgb(255, 180, 84)",
+      },
+    },
+    {
+      types: ["tag"],
+      style: {
+        color: "rgb(158, 255, 255)",
+      },
+    },
+    {
+      types: ["keyword"],
+      style: {
+        color: "rgb(255, 157, 0)",
+        fontStyle: "italic",
+      },
+    },
+  ],
+};
 
 module.exports = theme;
